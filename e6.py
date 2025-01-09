01ac106d82d417976afde9d49e665506598c4ae5
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-09 09:45:44 +0100

    Ejercicio 6 hecho

---
+++ e6.py
@@ -0,0 +1,9 @@
+n1 = int(input("Introduzca el primer número. "))
+n2 = int(input("Introduzca el segundo número. "))
+if (n1>n2):
+    print("El número", n1, "es mayor que", n2)
+else:
+    if (n1<n2):
+        print("El número", n2, "es mayor que", n1)
+else:
+    print("Los números son iguales.")
