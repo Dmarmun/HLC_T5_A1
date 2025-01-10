bcfc7eadd133d0b6aa5c7bcbd0c21dc37157ba5f
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-10 12:06:24 +0100

    Ejercicio 2 optimizado y ejercicio 7 hecho

--- e2.py
+++ e2.py
@@ -1,4 +1,3 @@
 n1 = int(input("Introduzca el primer número a sumar. "))
 n2 = int(input("Introduzca el segundo número a sumar. "))
-suma = n1 + n2
-print("La suma de los número es", suma)
+print("La suma de los número es", (n1+n2))
