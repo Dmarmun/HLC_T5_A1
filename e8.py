d9e502982a2a45827e5c6a2541c9ae64d8492f6f
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-10 12:59:54 +0100

    Ejercicio 8 hecho

---
+++ e8.py
@@ -0,0 +1,5 @@
+b = int(input("Introduzca la base del triángulo: "))
+a = int(input("Introduzca la altura del triángulo: "))
+area = (b*a)/2
+p = int(input("Introduzca el precio por metro cuadrado: "))
+print("El area del triángulo es", area, "y su precio por metro cuadrado es", (area*p))
